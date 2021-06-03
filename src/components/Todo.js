a33e14189db340ce9b80800111445799703b4b50
dude lao <brycelipscomb@gmail.com>
2021-06-02 22:21:55 -0400

    got the ToDo's styled to an acceptable place

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -35,21 +35,25 @@ export default function Todo({todo, ind, moveTodo, removeTodo, editTodo}){
             <button className="btn-complete">
                 <IconCheckCircle />
             </button>
-            <span title={`Description: ${todo.todoDescription}`}>
-                {todo.todoTitle}
-            </span>
-            <button className="btn-delete" onClick={deleteTodo}>
-                <IconDelete />
-            </button>
-            <button className="btn-move" onClick={moveUp}>
-                <IconArrowUp />
-            </button>
-            <button className="btn-move" onClick={moveDown}>
-                <IconArrowDown />
-            </button>
-            <button onClick={toggleEdit}>
-                Edit
-            </button>
+            <div className="todo-txt">
+                <span title={`Description: ${todo.todoDescription}`}>
+                    {todo.todoTitle}
+                </span>
+            </div>
+            <div className="todo-btn-bar">
+                <button className="btn-delete" onClick={deleteTodo}>
+                    <IconDelete />
+                </button>
+                <button className="btn-move" onClick={moveUp}>
+                    <IconArrowUp />
+                </button>
+                <button className="btn-move" onClick={moveDown}>
+                    <IconArrowDown />
+                </button>
+                <button onClick={toggleEdit}>
+                    Edit
+                </button>
+            </div>
         </div>
     )
 }
